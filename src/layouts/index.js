88c7613cf711cc86d9621d66fbb22efd706ada9d
dany <1574839461@qq.com>
2018-12-06 22:54:04 +0800

    feat:

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,18 +6,20 @@ class Layout extends Component {
     window.onload = function() {
       var top1 = 100;
       var nav = document.getElementById("nav");
-      window.onscroll = function() {
-        var top2 =
-          document.body.scrollTop || document.documentElement.scrollTop;
-        if (top2 > top1) {
-          nav.style =
-            " height:53px; width:100%;  position:fixed; top:-200px;left:0; transition:0.6s;";
-        } else {
-          nav.style =
-            " height:53px; width:100%;     opacity: 0.8;  position:fixed; top:0;left:0; transition:0.6s;";
-        }
-        // top1 = top2;
-      };
+      if (nav) {
+        window.onscroll = function() {
+          var top2 =
+            document.body.scrollTop || document.documentElement.scrollTop;
+          if (top2 > top1) {
+            nav.style =
+              " height:53px; width:100%;  position:fixed; top:-200px;left:0; transition:0.6s;";
+          } else {
+            nav.style =
+              " height:53px; width:100%;     opacity: 0.8;  position:fixed; top:0;left:0; transition:0.6s;";
+          }
+          // top1 = top2;
+        };
+      }
     };
   }
 
